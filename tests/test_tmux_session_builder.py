fbe406cb1f1f517f54f532fefcab5cc2a816a246
Milind Shakya <milind.shakya@dealertrack.com>
2015-02-15 23:24:20 -0500

    Added tests

---
+++ tests/test_tmux_session_builder.py
@@ -0,0 +1,32 @@
+import unittest
+import mock
+import simplejson as json
+
+from tmuxipy.main import TmuxSessionBuilder
+
+
+TESTING_MODULE = 'tmuxipy.tmux_session_builder'
+
+class TestTmuxSessionBuilder(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @mock.patch(TESTING_MODULE + '.Session.create')
+    @mock.patch(TESTING_MODULE + '._execute')
+    @mock.patch(TESTING_MODULE + '.kaptan.Kaptan.import_config')
+    def test_build(self,
+                   mock__execute,
+                   mock_kaptan_import_config,
+                   mock_session_create):
+        config = json.dumps({"session_name": "test"})
+        tmux_session_builder = TmuxSessionBuilder(config=config)
+        mock_kaptan_import_config.return_value = config
+        tmux_session_builder.build()
+        self.assertTrue(mock_kaptan_import_config.called)
+        self.assertTrue(mock_session_create.called)
+
+
+
+
+
