899a333ece41d6b64905c1f18625b1beef789b47
Milind Shakya <milind.shakya@dealertrack.com>
2015-02-21 00:20:14 -0500

    Added test for create and init for Session.

--- tests/test_tmux_session_builder.py
+++ tests/test_tmux_session_builder.py
@@ -2,7 +2,10 @@ import unittest
 import mock
 import simplejson as json
 
-from tmuxipy.main import TmuxSessionBuilder
+from tmuxipy.tmux_session_builder import (
+    TmuxSessionBuilder,
+    Session
+)
 
 
 TESTING_MODULE = 'tmuxipy.tmux_session_builder'
@@ -25,3 +28,44 @@ class TestTmuxSessionBuilder(unittest.TestCase):
         tmux_session_builder.build()
         self.assertTrue(mock_kaptan_import_config.called)
         self.assertTrue(mock_session_create.called)
+
+
+class TestSession(unittest.TestCase):
+
+    def setUp(self):
+        self.config = {'windows': [{'a': 'b'}, {'c': 'd'}]}
+        self.session = Session(
+            'Test',
+            self.config,
+            mock.sentinel.tmux
+        )
+
+    def test__init(self):
+        test_config = {'a': 'b'}
+        session_a = Session(
+            'Test',
+            test_config,
+            mock.sentinel.tmux
+        )
+        self.assertEqual('Test', session_a.name)
+        self.assertEqual(test_config, session_a.config)
+        self.assertEqual(mock.sentinel.tmux, session_a.tmux)
+        self.assertEqual({}, session_a.windows)
+
+    @mock.patch(TESTING_MODULE + '.Window')
+    @mock.patch.object(Session, '_sanity_check')
+    @mock.patch.object(Session, '_execute_pre_commands')
+    @mock.patch.object(Session, '_attach_to_session')
+    def test_create(self,
+                    mock__attach_to_session,
+                    mock__execute_pre_commands,
+                    mock__sanity_check,
+                    mock_window):
+        self.session.create()
+        mock__sanity_check.assert_called_once_with()
+        mock__execute_pre_commands.assert_called_once_with()
+        mock__attach_to_session.assert_called_once_with()
+
+
+
+
